241db24c64208ae7922da99a23aa997ad0dcb51b
eschnei2 <everettschneider@gmail.com>
2021-07-06 14:44:51 -0500

    section one done

---
+++ src/components/People/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employees.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Hannah McBeth</h3>
+        <div className="employee__address">Nashville Kennels North</div>
+    </section>
+)
