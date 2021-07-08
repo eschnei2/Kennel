ae660bbd92a554fa2a72a256d171f9c61fd08397
eschnei2 <everettschneider@gmail.com>
2021-07-08 10:20:04 -0500

    5donemaybe

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,49 +1,11 @@
 import React from "react"
-import { AnimalCard } from "./animal/AnimalCard"
-import "./animal/Animal.css"
-import { EmployeeCard } from "./People/EmployeeCard"
-import "./People/Employees.css"
-import { LocationCard } from "./Locations/LocationCard"
-import "./Locations/Locations.css"
-import { CustomerCard } from "./Customers/CustomerCard"
-import "./Customers/Customer.css"
-import { PropsAndState } from "./PropsAndState"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+import "./Kennel.css"
 
-//Look carefully at the <article> tag. In React, we add classes to a component with `className` instead of `class`.
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <PropsAndState yourName="Everettgit" />
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-        <h2>People</h2>
-        <article className="employees">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-        </article>
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationCard />
-            <LocationCard />
-        </article>
-        <h2>Customers</h2>
-        <article className="animals">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-        </article>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
